b3d7d87501fdfdb75fefb488cce0dec85a62ab78
Nathan Phame <diyf2021@gmail.com>
2021-05-31 16:53:08 -0400

    Added handleClick and handleRightClick

--- src/App.jsx
+++ src/App.jsx
@@ -30,18 +30,58 @@ export class App extends Component {
     this.setState(game)
   }
 
-  render() {
-    const grid = this.state.board.map((row, rowIndex) => {
-      return row.map((cell, colIndex) => {
-        return <li key={colIndex}>{cell}</li>
-      })
-    })
+  handleClickCell = async (rowIndex, colIndex) => {
+    const body = { row: rowIndex, col: colIndex }
 
+    const response = await fetch(
+      `https://minesweeper-api.herokuapp.com/games/${this.state.id}/check`,
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(body),
+      }
+    )
+    const game = await response.json()
+    this.setState(game)
+  }
+
+  handleRightClickCell = async (rowIndex, colIndex) => {
+    const body = { row: rowIndex, col: colIndex }
+
+    const response = await fetch(
+      `https://minesweeper-api.herokuapp.com/games/${this.state.id}/flag`,
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(body),
+      }
+    )
+    const game = await response.json()
+    this.setState(game)
+  }
+
+  render() {
     return (
       <main>
         <h1>Minesweeper</h1>
         <button onClick={this.handleNewGame}>New Easy Game</button>
-        <ul>{grid}</ul>
+        <ul>
+          {this.state.board.map((row, rowIndex) => {
+            return row.map((cell, colIndex) => {
+              return (
+                <li
+                  key={colIndex}
+                  onClick={() => this.handleClickCell(rowIndex, colIndex)}
+                  onContextMenu={() =>
+                    this.handleRightClickCell(rowIndex, colIndex)
+                  }
+                >
+                  {cell}
+                </li>
+              )
+            })
+          })}
+        </ul>
       </main>
     )
   }
